ef0a292c9e806970c1017c179d6e793872806f05
zvikabh <zvikabh@gmail.com>
2014-11-13 14:08:08 +0200

    Completed initial code

--- src/zvikabh/rccarcontroller/MainActivity.java
+++ src/zvikabh/rccarcontroller/MainActivity.java
@@ -3,13 +3,17 @@ package zvikabh.rccarcontroller;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Toast;
 
 
@@ -19,20 +23,35 @@ import android.widget.Toast;
  */
 public class MainActivity extends Activity {
 	
-	private static final String TAG = "MainActivity";
+	private static final String TAG = "rccarcontroller.MainActivity";
     
 	// Values used in Arduino Communicator
 	private static final int ARDUINO_USB_VENDOR_ID = 0x2341;
     private static final int ARDUINO_UNO_USB_PRODUCT_ID = 0x01;
     
     // Values used by my Arduino clone
-    private static final int ARDUINO_CLONE_VENDOR_ID = 0x0403;
-    private static final int ARDUINO_CLONE_PRODUCT_ID = 0x6001;
+    private static final int ARDUINO_CLONE_VENDOR_ID = 0x0403;  // 1027
+    private static final int ARDUINO_CLONE_PRODUCT_ID = 0x6001;  // 24577
+    
+    // SeekBars for the commands to the left and right motors.
+    private SeekBar mSeekbarLeft;
+    private SeekBar mSeekbarRight;
+    
+    // Receives notifications of changes in one of the seek bars.
+    // Which seek bar has changed is irrelevant, since we check the values of both of them
+    // whenever a change occurs.
+    private SeekBarChangeListener mSeekBarChangeListener = new SeekBarChangeListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mSeekbarLeft = (SeekBar) findViewById(R.id.seekBarLeft);
+        mSeekbarRight = (SeekBar) findViewById(R.id.seekBarRight);
+
+        mSeekbarLeft.setOnSeekBarChangeListener(mSeekBarChangeListener);
+        mSeekbarRight.setOnSeekBarChangeListener(mSeekBarChangeListener);
         
         findDevice();
     }
@@ -49,13 +68,17 @@ public class MainActivity extends Activity {
         for (UsbDevice usbDevice : usbDeviceList.values()) {
         	lastVendorId = usbDevice.getVendorId();
         	lastProductId = usbDevice.getProductId();
-        	Log.d(TAG, "VendorId: " + lastVendorId);
+        	Log.w(TAG, "VendorId: " + lastVendorId + "   ProductId: " + lastProductId);
             if ((lastVendorId == ARDUINO_USB_VENDOR_ID && lastProductId == ARDUINO_UNO_USB_PRODUCT_ID) ||
             		(lastVendorId == ARDUINO_CLONE_VENDOR_ID && lastProductId == ARDUINO_CLONE_PRODUCT_ID)) {
         		Log.d(TAG, "Found Arduino device");
         		Toast.makeText(getBaseContext(), "Found Arduino device", Toast.LENGTH_LONG).show();
-        		// TODO: Get permission for device, and pass it to the service.
-        		// See ArduinoCommunicatorActivity.java:115.
+
+        		// Ask the user for permission to access the device.
+        		// Send the response to the service, to start up the USB access.
+                Intent startIntent = new Intent(getApplicationContext(), RCCarControllerService.class);
+                PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 0, startIntent, 0);
+                usbManager.requestPermission(usbDevice, pendingIntent);
         		return true;
             }
         }
@@ -82,5 +105,37 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Class for handling notifications of changes to one of the motor's seek bars.
+     */
+    private class SeekBarChangeListener implements OnSeekBarChangeListener {
+    	
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+			int leftPos = mSeekbarLeft.getProgress() - mSeekbarLeft.getMax() / 2;
+			int rightPos = mSeekbarRight.getProgress() - mSeekbarRight.getMax() / 2;
+			
+			byte[] data = new byte[4];
+			data[0] = (byte) (leftPos & 0xFF);
+			data[1] = (byte) ((leftPos >> 8) & 0xFF);
+			data[2] = (byte) (rightPos & 0xFF);
+			data[3] = (byte) ((rightPos >> 8) & 0xFF);
+			
+			Intent sendDataIntent = new Intent(getApplicationContext(), RCCarControllerService.class);
+			sendDataIntent.setAction(RCCarControllerService.SEND_DATA_INTENT);
+			sendDataIntent.putExtra(RCCarControllerService.DATA_EXTRA, data);
+			startService(sendDataIntent);
+		}
+	
+		@Override
+		public void onStartTrackingTouch(SeekBar arg0) {
+		}
+	
+		@Override
+		public void onStopTrackingTouch(SeekBar arg0) {
+		}
+		
     }
 }
--- src/zvikabh/rccarcontroller/RCCarControllerService.java
+++ src/zvikabh/rccarcontroller/RCCarControllerService.java
@@ -1,12 +1,21 @@
 package zvikabh.rccarcontroller;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.usb.UsbConstants;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,9 +23,24 @@ public class RCCarControllerService extends Service {
 	
 	private static final String TAG = "RCCarControllerService";
 	static final String SEND_DATA_INTENT = "zvikabh.rccarcontroller.intent.SEND_DATA";
+    final static String DATA_SENT_INTERNAL_INTENT = "zvikabh.rccarcontroller.internal.intent.DATA_SENT";
 	static final String DATA_EXTRA = "zvikabh.rccarcontroller.extra.DATA";
+	
+	static final int BAUD_RATE = 19200;
 
-	@Override
+	// Is the service currently running?
+    private volatile boolean mIsRunning = false;
+    
+    // The Arduino USB device.
+    private volatile UsbDevice mUsbDevice = null;
+    private volatile UsbDeviceConnection mUsbConnection = null;
+    private volatile UsbEndpoint mInUsbEndpoint = null;
+    private volatile UsbEndpoint mOutUsbEndpoint = null;
+    
+    // Thread used for sending data to the Arduino.
+    private SenderThread mSenderThread;
+
+    @Override
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
@@ -38,8 +62,145 @@ public class RCCarControllerService extends Service {
         super.onDestroy();
         unregisterReceiver(mReceiver);
     }
+    
+    @Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand() " + intent + " " + flags + " " + startId);
+        
+        if (mIsRunning) {
+            Log.d(TAG, "Service already running.");
+            return Service.START_REDELIVER_INTENT;
+        }
+        
+        Log.d(TAG, "Starting service");
 
-    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        mIsRunning = true;
+        
+        if (!intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+            Log.w(TAG, "Permission to access USB device was denied");
+            Toast.makeText(getBaseContext(), "Permission to access USB device was denied", Toast.LENGTH_LONG).show();
+            stopSelf();
+            return Service.START_REDELIVER_INTENT;
+        }
+
+        mUsbDevice = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        Log.d(TAG, "Permission granted: mUsbDevice=" + mUsbDevice);
+        
+        if (!initDevice()) {
+            Log.e(TAG, "Init of device failed!");
+            Toast.makeText(getBaseContext(), "Init of device failed!", Toast.LENGTH_LONG).show();
+            stopSelf();
+            return Service.START_REDELIVER_INTENT;
+        }
+        
+        startSenderThread();
+
+        Log.d(TAG, "Service started successfully!");
+
+        // Indicates that the service should be restarted repeatedly as long as it has intents to process.
+        return Service.START_REDELIVER_INTENT;
+	}
+    
+    private boolean initDevice() {
+        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mUsbConnection = usbManager.openDevice(mUsbDevice);
+        if (mUsbConnection == null) {
+            Log.e(TAG, "Opening USB device failed!");
+            return false;
+        }
+        UsbInterface usbInterface = mUsbDevice.getInterface(1);
+        if (!mUsbConnection.claimInterface(usbInterface, true)) {
+            Log.e(TAG, "Claiming interface failed!");
+            mUsbConnection.close();
+            return false;
+        }
+
+        // Arduino USB serial converter setup
+        // Set control line state
+        mUsbConnection.controlTransfer(0x21, 0x22, 0, 0, null, 0, 0);
+        // Set line encoding.
+        mUsbConnection.controlTransfer(0x21, 0x20, 0, 0, getLineEncoding(BAUD_RATE), 7, 0);
+
+        for (int i = 0; i < usbInterface.getEndpointCount(); i++) {
+            if (usbInterface.getEndpoint(i).getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
+                if (usbInterface.getEndpoint(i).getDirection() == UsbConstants.USB_DIR_IN) {
+                    mInUsbEndpoint = usbInterface.getEndpoint(i);
+                } else if (usbInterface.getEndpoint(i).getDirection() == UsbConstants.USB_DIR_OUT) {
+                    mOutUsbEndpoint = usbInterface.getEndpoint(i);
+                }
+            }
+        }
+
+        if (mInUsbEndpoint == null) {
+            Log.e(TAG, "No in endpoint found!");
+            Toast.makeText(getBaseContext(), "no_in_endpoint_found", Toast.LENGTH_LONG).show();
+            mUsbConnection.close();
+            return false;
+        }
+
+        if (mOutUsbEndpoint == null) {
+            Log.e(TAG, "No out endpoint found!");
+            Toast.makeText(getBaseContext(), "no_out_endpoint_found", Toast.LENGTH_LONG).show();
+            mUsbConnection.close();
+            return false;
+        }
+
+        return true;
+    }
+    
+    private byte[] getLineEncoding(int baudRate) {
+        final byte[] lineEncodingRequest = { (byte) 0x80, 0x25, 0x00, 0x00, 0x00, 0x00, 0x08 };
+        
+        lineEncodingRequest[0] = (byte) (baudRate & 0xFF);
+        lineEncodingRequest[1] = (byte) ((baudRate >> 8) & 0xFF);
+        lineEncodingRequest[2] = (byte) ((baudRate >> 16) & 0xFF);
+
+        return lineEncodingRequest;
+    }
+
+    private void startSenderThread() {
+    	if (mSenderThread != null) {
+    		Toast.makeText(getBaseContext(), "Sender thread started twice!", Toast.LENGTH_LONG).show();
+    	}
+        mSenderThread = new SenderThread();
+        mSenderThread.start();
+    }
+
+    private class SenderThread extends Thread {
+        public Handler mHandler;
+
+        public SenderThread() {
+            super("arduino_sender");
+        }
+        
+        @SuppressLint("HandlerLeak") 
+        public void run() {
+            Looper.prepare();
+
+            mHandler = new Handler() {
+                public void handleMessage(Message msg) {
+                    Log.d(TAG, "handleMessage() " + msg.what);
+                    if (msg.what == 10) {
+                        final byte[] dataToSend = (byte[]) msg.obj;
+
+                        Log.d(TAG, "calling bulkTransfer() out");
+                        final int len = mUsbConnection.bulkTransfer(mOutUsbEndpoint, dataToSend, dataToSend.length, 0);
+                        Log.d(TAG, len + " of " + dataToSend.length + " sent.");
+                        Intent sendIntent = new Intent(DATA_SENT_INTERNAL_INTENT);
+                        sendIntent.putExtra(DATA_EXTRA, dataToSend);
+                        sendBroadcast(sendIntent);
+                    } else if (msg.what == 11) {
+                        Looper.myLooper().quit();
+                    }
+                }
+            };
+
+            Looper.loop();
+            Log.i(TAG, "sender thread stopped");
+        }
+    }
+    
+	BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
         	String action = intent.getAction();
@@ -57,8 +218,8 @@ public class RCCarControllerService extends Service {
                     return;
                 }
 
-                // TODO: Send data in sender thread.
-                // mSenderThread.mHandler.obtainMessage(10, dataToSend).sendToTarget();
+                // Send data in sender thread.
+                mSenderThread.mHandler.obtainMessage(10, dataToSend).sendToTarget();
         	}
         }		
 	};
