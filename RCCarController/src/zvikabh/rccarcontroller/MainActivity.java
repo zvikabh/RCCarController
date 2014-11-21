2ac37d037c472e68c4f999ece729274abbb5234f
zvikabh <zvikabh@gmail.com>
2014-11-21 19:01:02 +0200

    Starting to split to two separate apps. Created a RCCarReceiver app, which will receive commands from the RCCarController (through the internet), and pass them on to the Arduino.

--- RCCarController/src/zvikabh/rccarcontroller/MainActivity.java
+++ RCCarController/src/zvikabh/rccarcontroller/MainActivity.java
@@ -30,25 +30,22 @@ import com.hoho.android.usbserial.driver.UsbSerialProber;
  * Portions taken from Arduino Communicator, https://github.com/jeppsson/Arduino-Communicator
  */
 public class MainActivity extends Activity {
-	
-	private static final String TAG = "rccarcontroller.MainActivity";
-	
-	private static final String ACTION_USB_PERMISSION = "zvikabh.rccarcontroller.USB_PERMISSION";
-    
-	// Values used in Arduino Communicator
-	private static final int ARDUINO_USB_VENDOR_ID = 0x2341;
-	private static final int ARDUINO_UNO_USB_PRODUCT_ID = 0x0001;
 
-	// Values used by my Arduino clone
-	private static final int ARDUINO_CLONE_VENDOR_ID = 0x0403;  // 1027
-	private static final int ARDUINO_CLONE_PRODUCT_ID = 0x6001;  // 24577
+    private static final String TAG = "rccarcontroller.MainActivity";
 
-	/**
-     * SeekBars for the commands to the left and right motors.
-     */
+    private static final String ACTION_USB_PERMISSION = "zvikabh.rccarcontroller.USB_PERMISSION";
+
+    // Values used in Arduino Communicator
+    private static final int ARDUINO_USB_VENDOR_ID = 0x2341;
+    private static final int ARDUINO_UNO_USB_PRODUCT_ID = 0x0001;
+
+    // Values used by my Arduino clone
+    private static final int ARDUINO_CLONE_VENDOR_ID = 0x0403;  // 1027
+    private static final int ARDUINO_CLONE_PRODUCT_ID = 0x6001;  // 24577
+
     private TextView mTextviewArduinoResponse;
     private SpeedControllerView mViewSpeedController;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +53,7 @@ public class MainActivity extends Activity {
         Log.d(TAG, "onCreate");
 
         setContentView(R.layout.activity_main);
-        
+
         mTextviewArduinoResponse = (TextView) findViewById(R.id.textviewArduinoResponse);
         mViewSpeedController = (SpeedControllerView) findViewById(R.id.viewSpeedController);
         mViewSpeedController.setThrottleChangedListener(new ThrottleChangeListener());
@@ -68,56 +65,56 @@ public class MainActivity extends Activity {
         findDevice();
     }
 
-	@Override
-	protected void onDestroy() {
-		if (mUsbSerialPort != null) {
-			try {
-				mUsbSerialPort.close();
-			} catch (IOException e) {
-				Log.e(TAG, "Exception while closing port: " + e);
-			}
-		}
-		
-		mUsbSerialPort = null;
-		
-		unregisterReceiver(mBroadcastReceiver);
-		
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        if (mUsbSerialPort != null) {
+            try {
+                mUsbSerialPort.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Exception while closing port: " + e);
+            }
+        }
 
-	/**
+        mUsbSerialPort = null;
+
+        unregisterReceiver(mBroadcastReceiver);
+
+        super.onDestroy();
+    }
+
+    /**
      * Finds the Arduino USB device and asks the user for permission to use it.
      */
     private boolean findDevice() {
         UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(usbManager);
-        
+
         // Find the Arduino device.
         for (UsbSerialDriver driver : availableDrivers) {
-        	UsbDevice device = driver.getDevice();
-        	int vendorId = device.getVendorId();
-        	int productId = device.getProductId();
-        	if ((vendorId == ARDUINO_USB_VENDOR_ID && productId == ARDUINO_UNO_USB_PRODUCT_ID) ||
-        		(vendorId == ARDUINO_CLONE_VENDOR_ID && productId == ARDUINO_CLONE_PRODUCT_ID)) {
-        		Log.d(TAG, "Found Arduino device!");
-        		
-        		mUsbSerialDriver = driver;
-        		
-        		// Request user's permission to access the device.
-        		// Processing continues in mBroadcastReceiver after user grants permission.
-        		PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-        		usbManager.requestPermission(device, pendingIntent);
-        		return true;
-        	}
+            UsbDevice device = driver.getDevice();
+            int vendorId = device.getVendorId();
+            int productId = device.getProductId();
+            if ((vendorId == ARDUINO_USB_VENDOR_ID && productId == ARDUINO_UNO_USB_PRODUCT_ID) ||
+                    (vendorId == ARDUINO_CLONE_VENDOR_ID && productId == ARDUINO_CLONE_PRODUCT_ID)) {
+                Log.d(TAG, "Found Arduino device!");
+
+                mUsbSerialDriver = driver;
+
+                // Request user's permission to access the device.
+                // Processing continues in mBroadcastReceiver after user grants permission.
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                usbManager.requestPermission(device, pendingIntent);
+                return true;
+            }
         }
-        
-    	Log.e(TAG, "No Arduino devices found");
-    	Toast.makeText(this, "No Arduino devices found", Toast.LENGTH_LONG).show();
+
+        Log.e(TAG, "No Arduino devices found");
+        Toast.makeText(this, "No Arduino devices found", Toast.LENGTH_LONG).show();
         return false;
 
-	}
+    }
 
-	@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -141,172 +138,161 @@ public class MainActivity extends Activity {
      */
     private class ThrottleChangeListener implements SpeedControllerView.ThrottleChangedListener {
 
-		@Override
-		public void throttleChanged(float x, float y) {
-			if (mUsbSerialPort == null) {
-				Log.w(TAG, "Cannot update Arduino - connection to Arduino was unsuccessful.");
-				return;
-			}
+        @Override
+        public void throttleChanged(float x, float y) {
+            if (mUsbSerialPort == null) {
+                Log.w(TAG, "Cannot update Arduino - connection to Arduino was unsuccessful.");
+                return;
+            }
 
-			// Maximum power to deliver to each motor, on a scale of [0,1].
-			final double maxPower = Math.min(1, Math.sqrt(x*x + y*y));
+            // Maximum power to deliver to each motor, on a scale of [0,1].
+            final double maxPower = Math.min(1, Math.sqrt(x*x + y*y));
 
-			// Angle in which car should move:
-			// -pi:   turn left
-			// -pi/2: backward
-			// 0:     turn right
-			// pi/2:  forward
-			// pi:    turn left
-			final double angle = Math.atan2(-y, x);
-			
-			final double pi = Math.PI;
-			
-			// Fraction of max power to deliver to the left and right motors.
-			// Each motor (separately) is in the range [-1,1].
-			double leftPower, rightPower;
-			if (angle < -pi/2) {
-				// angle is in [-pi, -pi/2].
-				leftPower = -1.0;
-				rightPower = -(angle + pi*0.75) / (pi/4);
-			} else if (angle < 0) {
-				// angle is in [-pi/2, 0].
-				leftPower = (angle + pi/4) / (pi/4);
-				rightPower = -1.0;
-			} else if (angle < pi/2) {
-				// angle is in [0, pi/2].
-				leftPower = 1.0;
-				rightPower = (angle - pi/4) / (pi/4);
-			} else {
-				// angle is in [pi/2, pi].
-				leftPower = -(angle - pi*0.75) / (pi/4);
-				rightPower = 1.0;
-			}
-						
-			final short leftPowerLevel = (short) (400 * leftPower * maxPower);
-			final short rightPowerLevel = (short) (400 * rightPower * maxPower);
-			
-			final byte[] data = new byte[] { 0x7F, 0x7F, (byte)0x80, (byte)0x80, 0x00, 0x00, 0x00, 0x00 };
-			data[4] = (byte) (leftPowerLevel & 0xFF);
-			data[5] = (byte) ((leftPowerLevel >> 8) & 0xFF);
-			data[6] = (byte) (rightPowerLevel & 0xFF);
-			data[7] = (byte) ((rightPowerLevel >> 8) & 0xFF);
-			
-			mTextviewArduinoResponse.setText("L="+leftPowerLevel+"  R="+rightPowerLevel);
+            // Angle in which car should move:
+            // -pi:   turn left
+            // -pi/2: backward
+            // 0:     turn right
+            // pi/2:  forward
+            // pi:    turn left
+            final double angle = Math.atan2(-y, x);
 
-			try {
-				mUsbSerialPort.write(data, 500);
-			} catch (IOException e) {
-				Log.w(TAG, "Failed to write to USB serial port: " + e.toString());
-			}
-		}
-    	
+            final double pi = Math.PI;
+
+            // Fraction of max power to deliver to the left and right motors.
+            // Each motor (separately) is in the range [-1,1].
+            double leftPower, rightPower;
+            if (angle < -pi/2) {
+                // angle is in [-pi, -pi/2].
+                leftPower = -1.0;
+                rightPower = -(angle + pi*0.75) / (pi/4);
+            } else if (angle < 0) {
+                // angle is in [-pi/2, 0].
+                leftPower = (angle + pi/4) / (pi/4);
+                rightPower = -1.0;
+            } else if (angle < pi/2) {
+                // angle is in [0, pi/2].
+                leftPower = 1.0;
+                rightPower = (angle - pi/4) / (pi/4);
+            } else {
+                // angle is in [pi/2, pi].
+                leftPower = -(angle - pi*0.75) / (pi/4);
+                rightPower = 1.0;
+            }
+
+            final short leftPowerLevel = (short) (400 * leftPower * maxPower);
+            final short rightPowerLevel = (short) (400 * rightPower * maxPower);
+
+            final byte[] data = new byte[] { 0x7F, 0x7F, (byte)0x80, (byte)0x80, 0x00, 0x00, 0x00, 0x00 };
+            data[4] = (byte) (leftPowerLevel & 0xFF);
+            data[5] = (byte) ((leftPowerLevel >> 8) & 0xFF);
+            data[6] = (byte) (rightPowerLevel & 0xFF);
+            data[7] = (byte) ((rightPowerLevel >> 8) & 0xFF);
+
+            mTextviewArduinoResponse.setText("L="+leftPowerLevel+"  R="+rightPowerLevel);
+
+            try {
+                mUsbSerialPort.write(data, 500);
+            } catch (IOException e) {
+                Log.w(TAG, "Failed to write to USB serial port: " + e.toString());
+            }
+        }
+
     }
-    
-	final private static char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-	private static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-	        hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-	    }
-	    return new String(hexChars);
-	}
 
-	private volatile UsbSerialPort mUsbSerialPort;
-	private volatile UsbSerialDriver mUsbSerialDriver;
-	
-	/**
-	 * Completes the setup of the connection to the Arduino device,
-	 * after the user has granted permission to access it.
-	 * At this point, mUsbSerialDriver is set to the Arduino device.
-	 * Upon successful completion, mUsbSerialPort will point to the (opened) port.
-	 * Upon failure, mUsbSerialPort will be null.
-	 * @return true iff success
-	 */
-	private boolean openUsbSerialPort() {
+    private volatile UsbSerialPort mUsbSerialPort;
+    private volatile UsbSerialDriver mUsbSerialDriver;
+
+    /**
+     * Completes the setup of the connection to the Arduino device,
+     * after the user has granted permission to access it.
+     * At this point, mUsbSerialDriver is set to the Arduino device.
+     * Upon successful completion, mUsbSerialPort will point to the (opened) port.
+     * Upon failure, mUsbSerialPort will be null.
+     * @return true iff success
+     */
+    private boolean openUsbSerialPort() {
         UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         UsbDeviceConnection connection = usbManager.openDevice(mUsbSerialDriver.getDevice());
         if (connection == null) {
-        	// Strange - we were supposed to have already gotten permission.
-        	Log.e(TAG, "No permission to access device");
-        	Toast.makeText(this, "No permission to access device", Toast.LENGTH_LONG).show();
-        	mUsbSerialPort = null;
-        	return false;
+            // Strange - we were supposed to have already gotten permission.
+            Log.e(TAG, "No permission to access device");
+            Toast.makeText(this, "No permission to access device", Toast.LENGTH_LONG).show();
+            mUsbSerialPort = null;
+            return false;
         }
-        
+
         List<UsbSerialPort> ports = mUsbSerialDriver.getPorts();
         if (ports.size() == 0) {
-        	Log.e(TAG, "Device has 0 ports");
-        	Toast.makeText(this, "Device has 0 ports", Toast.LENGTH_LONG).show();
-        	mUsbSerialPort = null;
-        	return false;
+            Log.e(TAG, "Device has 0 ports");
+            Toast.makeText(this, "Device has 0 ports", Toast.LENGTH_LONG).show();
+            mUsbSerialPort = null;
+            return false;
         }
-        
+
         mUsbSerialPort = ports.get(0);
-        
+
         try {
-			mUsbSerialPort.open(connection);
-			mUsbSerialPort.setParameters(19200, UsbSerialPort.DATABITS_8, 
-					UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-		} catch (IOException e) {
-			mUsbSerialPort = null;
-        	Log.e(TAG, "Could not open port");
-        	Toast.makeText(this, "Could not open port", Toast.LENGTH_LONG).show();
-        	return false;
-		}
-        
+            mUsbSerialPort.open(connection);
+            mUsbSerialPort.setParameters(19200, UsbSerialPort.DATABITS_8, 
+                    UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (IOException e) {
+            mUsbSerialPort = null;
+            Log.e(TAG, "Could not open port");
+            Toast.makeText(this, "Could not open port", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
         // Start receiver thread.
         mReceiverThread.start();
-        
-        return true;
-	}
-	
-	private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
 
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			final String action = intent.getAction();
-			if (action.equals(ACTION_USB_PERMISSION)) {
-				if (!intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-					Log.e(TAG, "User did not grant permission to use the device.");
-					return;
-				}
-				openUsbSerialPort();
-			}
-		}
-		
-	};
-	
-	private final Thread mReceiverThread = new Thread() {
-		
-		public void run() {
-			while (mUsbSerialPort != null) {
-				byte[] data = new byte[4096];
-				int nBytesRead = 0;
-				try {
-					nBytesRead = mUsbSerialPort.read(data, 1000);
-				} catch (IOException e) {
-					Log.w(TAG, "Error reading from USB serial port: " + e);
-					continue;
-				}
-				if (nBytesRead > 0) {
-					String receivedString;
-					try {
-						receivedString = new String(data, "UTF-8");
-					} catch (UnsupportedEncodingException e) {
-						receivedString = "Invalid UTF-8 received";
-					}
-					final String arduinoResponse = receivedString;
-					mTextviewArduinoResponse.post(new Runnable() {
-						public void run() {
-							mTextviewArduinoResponse.setText(arduinoResponse);
-						}
-					});
-				}
-			}
-			Log.d(TAG, "Receiver thread stopped: mUsbSerialPort == null");
-		}
-		
-	};
+        return true;
+    }
+
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (action.equals(ACTION_USB_PERMISSION)) {
+                if (!intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    Log.e(TAG, "User did not grant permission to use the device.");
+                    return;
+                }
+                openUsbSerialPort();
+            }
+        }
+
+    };
+
+    private final Thread mReceiverThread = new Thread() {
+
+        public void run() {
+            while (mUsbSerialPort != null) {
+                byte[] data = new byte[4096];
+                int nBytesRead = 0;
+                try {
+                    nBytesRead = mUsbSerialPort.read(data, 1000);
+                } catch (IOException e) {
+                    Log.w(TAG, "Error reading from USB serial port: " + e);
+                    continue;
+                }
+                if (nBytesRead > 0) {
+                    String receivedString;
+                    try {
+                        receivedString = new String(data, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        receivedString = "Invalid UTF-8 received";
+                    }
+                    final String arduinoResponse = receivedString;
+                    mTextviewArduinoResponse.post(new Runnable() {
+                        public void run() {
+                            mTextviewArduinoResponse.setText(arduinoResponse);
+                        }
+                    });
+                }
+            }
+            Log.d(TAG, "Receiver thread stopped: mUsbSerialPort == null");
+        }
+
+    };
 }
--- RCCarController/src/zvikabh/rccarcontroller/SpeedControllerView.java
+++ RCCarController/src/zvikabh/rccarcontroller/SpeedControllerView.java
@@ -15,132 +15,132 @@ import android.view.View;
  */
 public class SpeedControllerView extends View {
 
-	public SpeedControllerView(Context context) {
-		super(context);
-		init();
-	}
+    public SpeedControllerView(Context context) {
+        super(context);
+        init();
+    }
 
-	public SpeedControllerView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
+    public SpeedControllerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
-	public SpeedControllerView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init();
-	}
-	
-	public void setThrottleChangedListener(ThrottleChangedListener listener) {
-		mThrottleChangedListener = listener;
-	}
-	
-	public interface ThrottleChangedListener {
-		/**
-		 * Called whenever the throttle position has changed.
-		 * Will be called many times repeatedly if the throttle is dragged.
-		 * @param x New X position, between -1 and 1.
-		 * @param y New Y position, between -1 and 1.
-		 */
-		public void throttleChanged(float x, float y);
-	}
+    public SpeedControllerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
 
-	private void init() {
-		mPaintBlackPen = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mPaintBlackPen.setColor(0xff000000);
-		mPaintBlackPen.setStyle(Style.STROKE);
-		
-		mPaintThickBlackPen = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mPaintThickBlackPen.setColor(0xff000000);
-		mPaintThickBlackPen.setStyle(Style.STROKE);
-		mPaintThickBlackPen.setStrokeWidth(7);
+    public void setThrottleChangedListener(ThrottleChangedListener listener) {
+        mThrottleChangedListener = listener;
+    }
 
-		mPaintYellowFill = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mPaintYellowFill.setColor(0xffffffc0);
-		mPaintYellowFill.setStyle(Style.FILL);
-		
-		mPaintBlueFill = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mPaintBlueFill.setColor(0xff4040ff);
-		mPaintBlueFill.setStyle(Style.FILL);
-	}
-	
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		float xpad = (float) (getPaddingLeft() + getPaddingRight());
-		float ypad = (float) (getPaddingTop() + getPaddingBottom());
-		float ww = (float) w - xpad;
-		float hh = (float) h - ypad;
-		mRadius = Math.min(ww, hh) / 2;
-		mThrottleRadius = mRadius / 10;
-		mCenterX = ww / 2 + getPaddingLeft();
-		mCenterY = hh / 2 + getPaddingTop();
-		
-		resetThrottle();
-	}
-	
-	void resetThrottle() {
-		mThrottleX = mCenterX;
-		mThrottleY = mCenterY;
-	}
+    public interface ThrottleChangedListener {
+        /**
+         * Called whenever the throttle position has changed.
+         * Will be called many times repeatedly if the throttle is dragged.
+         * @param x New X position, between -1 and 1.
+         * @param y New Y position, between -1 and 1.
+         */
+        public void throttleChanged(float x, float y);
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		// Draw large yellow circle.
-		canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaintYellowFill);
-		canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaintBlackPen);
-		
-		// Draw throttle position.
-		canvas.drawLine(mCenterX, mCenterY, mThrottleX, mThrottleY, mPaintThickBlackPen);
-		canvas.drawCircle(mThrottleX, mThrottleY, mThrottleRadius, mPaintBlueFill);
-		canvas.drawCircle(mThrottleX, mThrottleY, mThrottleRadius, mPaintThickBlackPen);
-	}
-	
-	@SuppressLint("ClickableViewAccessibility") 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		float x, y;
-		
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-		case MotionEvent.ACTION_MOVE:
-			x = event.getX();
-			y = event.getY();
-			break;
-			
-		case MotionEvent.ACTION_UP:
-		default:
-			x = mCenterX;
-			y = mCenterY;
-			break;
-		}
-		
-		float dx = x - mCenterX;
-		float dy = y - mCenterY;
-		double radius = Math.sqrt(dx*dx + dy*dy);
-		if (radius > mRadius) {
-			dx *= (mRadius / radius);
-			dy *= (mRadius / radius);
-		}
-		
-		updateThrottlePos(mCenterX + dx, mCenterY + dy);
-		return true;
-	}
-	
-	private void updateThrottlePos(float newX, float newY) {
-		mThrottleX = newX;
-		mThrottleY = newY;
-		invalidate();
-		
-		if (mThrottleChangedListener != null) {
-			float normalizedX = (mThrottleX - mCenterX) / mRadius;
-			float normalizedY = (mThrottleY - mCenterY) / mRadius;
-			mThrottleChangedListener.throttleChanged(normalizedX, normalizedY);
-		}
-	}
-	
-	private ThrottleChangedListener mThrottleChangedListener;
+    private void init() {
+        mPaintBlackPen = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBlackPen.setColor(0xff000000);
+        mPaintBlackPen.setStyle(Style.STROKE);
 
-	private Paint mPaintBlackPen, mPaintThickBlackPen, mPaintYellowFill, mPaintBlueFill;
-	
-	private float mRadius, mCenterX, mCenterY;
-	private float mThrottleRadius, mThrottleX, mThrottleY;
+        mPaintThickBlackPen = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintThickBlackPen.setColor(0xff000000);
+        mPaintThickBlackPen.setStyle(Style.STROKE);
+        mPaintThickBlackPen.setStrokeWidth(7);
+
+        mPaintYellowFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintYellowFill.setColor(0xffffffc0);
+        mPaintYellowFill.setStyle(Style.FILL);
+
+        mPaintBlueFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBlueFill.setColor(0xff4040ff);
+        mPaintBlueFill.setStyle(Style.FILL);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        float xpad = (float) (getPaddingLeft() + getPaddingRight());
+        float ypad = (float) (getPaddingTop() + getPaddingBottom());
+        float ww = (float) w - xpad;
+        float hh = (float) h - ypad;
+        mRadius = Math.min(ww, hh) / 2;
+        mThrottleRadius = mRadius / 10;
+        mCenterX = ww / 2 + getPaddingLeft();
+        mCenterY = hh / 2 + getPaddingTop();
+
+        resetThrottle();
+    }
+
+    void resetThrottle() {
+        mThrottleX = mCenterX;
+        mThrottleY = mCenterY;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Draw large yellow circle.
+        canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaintYellowFill);
+        canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaintBlackPen);
+
+        // Draw throttle position.
+        canvas.drawLine(mCenterX, mCenterY, mThrottleX, mThrottleY, mPaintThickBlackPen);
+        canvas.drawCircle(mThrottleX, mThrottleY, mThrottleRadius, mPaintBlueFill);
+        canvas.drawCircle(mThrottleX, mThrottleY, mThrottleRadius, mPaintThickBlackPen);
+    }
+
+    @SuppressLint("ClickableViewAccessibility") 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x, y;
+
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN:
+        case MotionEvent.ACTION_MOVE:
+            x = event.getX();
+            y = event.getY();
+            break;
+
+        case MotionEvent.ACTION_UP:
+        default:
+            x = mCenterX;
+            y = mCenterY;
+            break;
+        }
+
+        float dx = x - mCenterX;
+        float dy = y - mCenterY;
+        double radius = Math.sqrt(dx*dx + dy*dy);
+        if (radius > mRadius) {
+            dx *= (mRadius / radius);
+            dy *= (mRadius / radius);
+        }
+
+        updateThrottlePos(mCenterX + dx, mCenterY + dy);
+        return true;
+    }
+
+    private void updateThrottlePos(float newX, float newY) {
+        mThrottleX = newX;
+        mThrottleY = newY;
+        invalidate();
+
+        if (mThrottleChangedListener != null) {
+            float normalizedX = (mThrottleX - mCenterX) / mRadius;
+            float normalizedY = (mThrottleY - mCenterY) / mRadius;
+            mThrottleChangedListener.throttleChanged(normalizedX, normalizedY);
+        }
+    }
+
+    private ThrottleChangedListener mThrottleChangedListener;
+
+    private Paint mPaintBlackPen, mPaintThickBlackPen, mPaintYellowFill, mPaintBlueFill;
+
+    private float mRadius, mCenterX, mCenterY;
+    private float mThrottleRadius, mThrottleX, mThrottleY;
 }
