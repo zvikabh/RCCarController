bd4fc8c200b574557572f5e62c19f9d246157884
zvikabh <zvikabh@gmail.com>
2014-11-24 14:53:06 +0200

    list ip addresses

--- RCCarReceiver/src/zvikabh/rccarreceiver/MainActivity.java
+++ RCCarReceiver/src/zvikabh/rccarreceiver/MainActivity.java
@@ -1,10 +1,19 @@
 package zvikabh.rccarreceiver;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,10 +23,36 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        mIPAddressesView = (ListView) findViewById(R.id.ipAddressesList);
+        findIPAddresses();
+        
         Intent serviceIntent = new Intent(getApplicationContext(), RCCarReceiverService.class);
         startService(serviceIntent);
     }
 
+    private void findIPAddresses() {
+        ArrayList<NetworkInterface> networkInterfaces = new ArrayList<NetworkInterface>();
+        final ArrayAdapter<String> displayData = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        try {
+            networkInterfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+        } catch (SocketException e) {
+            Log.e(TAG, "Cannot get network interfaces: " + e);
+            displayData.add("Cannot find IP addresses");
+        }
+        
+        for (NetworkInterface networkInterface : networkInterfaces) {
+            Log.d(TAG, "Found network interface: " + networkInterface.getDisplayName());
+            for (InetAddress ipAddress : Collections.list(networkInterface.getInetAddresses())) {
+                if (ipAddress.isLoopbackAddress()) {
+                    continue;
+                }
+                Log.d(TAG, "Found ip address: " + ipAddress.getHostAddress());
+                displayData.add(ipAddress.getHostAddress() + ":" + RCCarReceiverService.PORT);
+            }
+        }
+        
+        mIPAddressesView.setAdapter(displayData);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,4 +72,8 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    private ListView mIPAddressesView;
+    
+    private static final String TAG = "rccarreceiver.MainActivity";
 }
